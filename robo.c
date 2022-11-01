bf5faf474aadbb35b84bb41c6b9e72279921198e
Hi, my name is Bernardo! <90293389+beternus@users.noreply.github.com>
2022-11-01 19:04:12 -0300

    Update robo.c

--- robo.c
+++ robo.c
@@ -4,42 +4,39 @@ int IN3 = 8;
 int IN4 = 7;
 int SensorD = 10;        // sensor de linha da direita
 int SensorE = 11;       // sensor de linha da esquerda
-int E = 2;
-int D = 3;
+int SensorL = 6;    //define sensor lateral
+int SensorF = 13;  //sensor da frente
 
 //Carrega a biblioteca do sensor ultrassonico
-#include Ultrasonic ultrasonic(pino_trigger, pino_echo);
+//#include Ultrasonic ultrasonic(pino_trigger, pino_echo);
 
+//#include <NewPing.h>
 //Define os pinos para o trigger e echo
-#define pino_trigger 12
-#define pino_echo 13
+//#define TRIGGER_PIN 12
+//#define ECHO_PIN 13
+//#define MAX_DISTANCE 150
 
-//Inicializa o sensor nos pinos definidos acima
-Ultrasonic ultrasonic(pino_trigger, pino_echo);
+//NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-
-void setup()
-{
-  //Define os pinos como saida
+void setup(){
+  Serial.begin(9600);
+  //PinMode define os pinos como saida ou entrada
  pinMode(IN1, OUTPUT);
  pinMode(IN2, OUTPUT);
  pinMode(IN3, OUTPUT);
  pinMode(IN4, OUTPUT);
- pinMode(D, INPUT);
- pinMode(E, INPUT);
  pinMode(SensorD, INPUT);
  pinMode(SensorE, INPUT);
+ pinMode(SensorF, INPUT);
+ pinMode(SensorL, INPUT);
 
 //inicia parado
- digitalWrite(IN1, LOW);
+ digitalWrite(IN1, LOW);  
  digitalWrite(IN2, LOW);
  digitalWrite(IN3, LOW);
  digitalWrite(IN4, LOW);
- digitalWrite(E, LOW);
- digitalWrite(D, LOW);
-
- Serial.begin(115200);
- Serial.println("Lendo dados do sensor…");
+// digitalWrite(SensorD, LOW);
+// digitalWrite(SensorE, LOW);
 }
 
 void frente()
@@ -80,95 +77,46 @@ void tras()
 
 void parar()
 {
- digitalWrite(IN1, HIGH);
- digitalWrite(IN2, HIGH);
- digitalWrite(IN3, HIGH);
- digitalWrite(IN4, HIGH);
+ digitalWrite(IN1, LOW);
+ digitalWrite(IN2, LOW);
+ digitalWrite(IN3, LOW);
+ digitalWrite(IN4, LOW);
  delay(200);
 }
 
 // so a preparacao
 void loop()
 {
+ // digitalRead(SensorD);   // lê o pino de entrada
+ // digitalRead(SensorE);
+ // digitalRead(SensorF);   // lê o pino de entrada
+//  digitalRead(SensorL);   // lê o pino de entrada
 
-  //Le as informacoes do sensor, em cm e pol
-  float cmMsec, inMsec;
-  cmMsec = ultrasonic.distanceRead(CM);
-  inMsec = ultrasonic.distanceRead(INC);
-
-  //Exibe informacoes no serial monitor
-  Serial.print(“Distancia em cm: “);
-  Serial.print(cmMsec);
-  Serial.print(” – Distancia em polegadas: “);
-  Serial.println(inMsec);
-
-  delay(1000);
-  
-//  delay(50);
-//  Serial.print("Ping: ");
-//  int dist = sonar.ping();
-//  Serial.print(sonar.convert_cm(dist)); //send ping, get distance
-//  Serial.println("us");
-  
-  
-  
-  frente();
-  delay(1000);
-//  esquerda();
-//  delay(1000);
-//  direita();
-//  delay(1000);
-  
-  Serial.println("loop normal");
-
-  do{
-
-  if((D == 1) && (E == 1))
- {
-    frente();
-     Serial.println("frente"); 
-  }
-  
-  else if ((D == 1)&&(E == 0))
+  Serial.print("Direita: ");
+  Serial.println( digitalRead(SensorD));
+  Serial.print("Esquerda: ");
+  Serial.println( digitalRead(SensorE));
+  Serial.print("Frente: ");
+  Serial.println( digitalRead(SensorF));
+  Serial.print("Lateral: ");
+  Serial.println( digitalRead(SensorL));
+/*
+   if ((SensorD == 1)&&(SensorE == 0))
   {
     esquerda();
-    Serial.println("esquerda"); 
+    Serial.println(D); 
 
   }
-  else if ((D == 0)&&(E == 1))
+   if ((SensorD == 0)&&(SensorE == 1))
   {
     direita();
-    Serial.println("direita"); 
+    Serial.println(D); 
   }
   
-  else if ((D == 1)&&(E == 1))
+  if ((D == 1)&&(E == 1))
   {
     parar();
-    Serial.println("parar"); 
-  }
-  }while (U==0);
-    if (U==1)
-  {
-    parar();
-  }
-  delay(100);
-}
-
-  }
-  else if ((D == 0)&&(E == 1))
-  {
-    direita();
-    Serial.println("direita"); 
-
-  }
-  else if ((D == 1)&&(E == 1))
-  {
-    parar();
-    Serial.println("parar"); 
-  }
-  if (U == 1)
-  {
-    parar();
-  }
-  delay(100);
+    Serial.println(D); 
+  }*/
+  delay(250);
 }

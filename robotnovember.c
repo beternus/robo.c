4d90e50208e15a1200dd028bc22800c1b4394880
Hi, my name is Bernardo! <90293389+beternus@users.noreply.github.com>
2022-11-10 17:57:02 -0300

    Update robotnovember.c

--- robotnovember.c
+++ robotnovember.c
@@ -2,16 +2,20 @@ int IN1 = 4;
 int IN2 = 5;
 int IN3 = 8;
 int IN4 = 7;
+
 int SensorD = 10;        // sensor de linha da direita
 int SensorE = 11;       // sensor de linha da esquerda
 int SensorL = 6;  //define sensor lateral
-int SensorF = 13;  //sensor da frente
+//int SensorF = 13;  //sensor da frente
+
 int E = 2;
 int D = 3; //REVISAR PABLO
-int F = 13;
+int ledplaca = 13;
 int L = 1;
 
-int contagemlateral;
+//int contagemlateral;
+int contador = 0;
+int contadorL = 0;
 
 void setup(){
   Serial.begin(9600);
@@ -23,111 +27,92 @@ void setup(){
  pinMode(IN4, OUTPUT);
  pinMode(D, INPUT);
  pinMode(E, INPUT);
+ pinMode(L, INPUT);
  
  pinMode(SensorD, INPUT);
  pinMode(SensorE, INPUT);
- pinMode(SensorF, INPUT);
+ pinMode(SensorL, INPUT);
 
 //inicia parado
  digitalWrite(IN1, LOW);
  digitalWrite(IN2, LOW);
  digitalWrite(IN3, LOW);
  digitalWrite(IN4, LOW);
- 
 }
 
 void frente()
 {
-  if (F == HIGH)
-  {
- Serial.println( digitalRead(SensorF));
+// Serial.println( digitalRead(SensorF));
  digitalWrite(IN1, HIGH);
  digitalWrite(IN2, LOW);
  digitalWrite(IN3, HIGH);
- digitalWrite(IN4, LOW);
- delay (30);
-  } 
+ digitalWrite(IN4, LOW); 
 }
 
 void esquerda()
 {
-   if (F == HIGH)
-  {
-Serial.println( digitalRead(SensorF));
+//Serial.println( digitalRead(SensorF));
  digitalWrite(IN1, HIGH);
  digitalWrite(IN2, LOW);
  digitalWrite(IN3, LOW);
  digitalWrite(IN4, HIGH);
- delay(30);
-  }
 }
 
 void direita()
 {
-   if (F == HIGH)
-  {
- Serial.println( digitalRead(SensorF));
+// Serial.println( digitalRead(SensorF));
  digitalWrite(IN1, LOW);
  digitalWrite(IN2, HIGH);
  digitalWrite(IN3, HIGH);
  digitalWrite(IN4, LOW);
- delay(30);
-  }
-}
-
-void tras()
-{
-   if (F == HIGH)
-  {
- digitalWrite(IN1, LOW);
- digitalWrite(IN2, HIGH);
- digitalWrite(IN3, LOW);
- digitalWrite(IN4, HIGH);
- delay (30);
-  }
-}
-
-void parar()
-{
- digitalWrite(IN1, HIGH);
- digitalWrite(IN2, HIGH);
- digitalWrite(IN3, HIGH);
- digitalWrite(IN4, HIGH);
- delay(30);
 }
 
 // so a preparacao
 void loop()
 {
-  delay(25);
+  delay(500);
   D = digitalRead(SensorD);   // lê o pino de entrada
   E = digitalRead(SensorE);
-  F = digitalRead(SensorF);
   L = digitalRead(SensorL);
-  
-  Serial.print("Direita: ");
-  Serial.println( digitalRead(SensorD));
-  Serial.print("Esquerda: ");
-  Serial.println( digitalRead(SensorE));
-  Serial.print("Frente: ");
-  Serial.println( digitalRead(SensorF));
-  Serial.print("Lateral: ");
-  Serial.println( digitalRead(SensorL));
 
- if ((D == LOW )&&(E == LOW))
+  if ((D == LOW )&&(E == LOW))
   {
     frente();    
-    Serial.println("frente"); 
+    Serial.println(" indo p frente"); 
   }
-  
-   if ((D == LOW)&&(E == HIGH))
+
+ if ((D == LOW)&&(E == HIGH))
   {
     direita();
-    Serial.println("direita"); 
+    Serial.println("indo p direita"); 
   }
-   if ((D == HIGH)&&(E == LOW))
+ if ((D == HIGH)&&(E == LOW))
   {
     esquerda();
-    Serial.println("esquerda"); 
+    Serial.println("indo p esquerda"); 
   }
+  else if ((D == HIGH)&&(E == HIGH))
+  {
+    contador++;
+    frente();
+    Serial.println(" indo p frente"); 
+  }
+  Serial.println(contador);
+  if(L == HIGH)
+  {
+    contadorL++;
+    frente();
+  }
+//ABAIXO MUDEI DE L PARA contador...
+  if (contador == 3)
+  {  
+    //NESSA PARTE AQUI O COLEGA ME AJUDOU A FAZER - CONSISTE EM UMA CONTAGEM DOS QUADRANTES PARA PODER GIRAR PARA A ESQUERDA
+    while(contadorL <= 4)
+    {
+      esquerda();
+      delay(50);
+      digitalWrite(13, HIGH);
+      contador = 4;
+    }
+  }    
 }
